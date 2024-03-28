f730a2d60fbe66d4c98f753220c55296e566dde4
Kola Karthik <120322998+karthik1726@users.noreply.github.com>
2024-03-28 10:23:53 +0530

    Update MessageSender.py

--- MessageSender.py
+++ MessageSender.py
@@ -1,7 +1,24 @@
 from twilio.rest import Client
 import subprocess
+import os
 
-output = subprocess.check_output(["python", "other_script.py"], text=True)
+def execute_python_file(file_path):
+   try:
+      completed_process = subprocess.run(['python', file_path], capture_output=True, text=True)
+      if completed_process.returncode == 0:
+         print("Execution successful.")
+         print("Output:")
+         print(completed_process.stdout)
+      else:
+         print(f"Error: Failed to execute '{file_path}'.")
+         print("Error output:")
+         print(completed_process.stderr)
+   except FileNotFoundError:
+      print(f"Error: The file '{file_path}' does not exist.")
+
+
+file_path = '/content/fubar.py'
+execute_python_file(file_path)
 
 account_sid = 'ACafd4d15e6c064ba01364c1735bf7865f'
 auth_token = '7209b3a932621ad118e908802549feef'
