fed2aa61075ac85739b906e2b6285c2dffd63c13
Kola Karthik <120322998+karthik1726@users.noreply.github.com>
2024-03-28 10:28:13 +0530

    Update MessageSender.py

--- MessageSender.py
+++ MessageSender.py
@@ -6,9 +6,11 @@ def execute_python_file(file_path):
    try:
       completed_process = subprocess.run(['python', file_path], capture_output=True, text=True)
       if completed_process.returncode == 0:
-         print("Execution successful.")
-         print("Output:")
-         print(completed_process.stdout)
+         message = client.messages.create(
+         from_='whatsapp:+14155238886',
+         body=completed_process.stdout,
+         to='whatsapp:+918790888152'
+         )
       else:
          print(f"Error: Failed to execute '{file_path}'.")
          print("Error output:")
@@ -17,17 +19,11 @@ def execute_python_file(file_path):
       print(f"Error: The file '{file_path}' does not exist.")
 
 
-file_path = '/content/fubar.py'
+file_path = 'CodeForces.py'
 execute_python_file(file_path)
 
 account_sid = 'ACafd4d15e6c064ba01364c1735bf7865f'
 auth_token = '7209b3a932621ad118e908802549feef'
 client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-  from_='whatsapp:+14155238886',
-  body=output.script(),
-  to='whatsapp:+918790888152'
-)
 
 print(message.sid)
