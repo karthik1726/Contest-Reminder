5b09a5aa2db8f3f9e591608deea67b6875be3cb7
Kola Karthik <120322998+karthik1726@users.noreply.github.com>
2024-03-29 16:29:54 +0530

    Update MessageSender.py

--- MessageSender.py
+++ MessageSender.py
@@ -1,28 +1,29 @@
-from twilio.rest import Client
-import subprocess
 import os
+import subprocess
+from twilio.rest import Client
 
-def execute_python_file(file_path):
-   try:
-      completed_process = subprocess.run(['python', file_path], capture_output=True, text=True)
-      if completed_process.returncode == 0:
-         message = client.messages.create(
-         from_='whatsapp:+14155238886',
-         body=completed_process.stdout,
-         to='whatsapp:+918790888152'
-         )
-      else:
-         print(f"Error: Failed to execute '{file_path}'.")
-         print("Error output:")
-         print(completed_process.stderr)
-   except FileNotFoundError:
-      print(f"Error: The file '{file_path}' does not exist.")
+def execute_CodeForces_Info(file_path):
+    try:
+        completed_process = subprocess.run(['python',file_path],capture_output=True,text=True)
+        if completed_process.returncode == 0:
+            print("Successfully Exicuted")
+            return completed_process.stdout
+        else:
+            print(f"Error: Failed to execute'{file_path}'.")
+            print(f"Error Output:")
+            print(completed_process.stderr)
+    except FileNotFoundError:
+        print(f"Error: The file'{file_path}' does not exist.")
 
-
+file_path = "C:\\Users\\karth\\OneDrive\\Desktop\\Contest Reminder\\ContestInfo\\codeforces.py"
 account_sid = 'ACafd4d15e6c064ba01364c1735bf7865f'
 auth_token = '7209b3a932621ad118e908802549feef'
 client = Client(account_sid, auth_token)
-file_path = 'CodeForces.py'
-execute_python_file(file_path)
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body= execute_CodeForces_Info(file_path),
+  to='whatsapp:+917075832552'
+)
 
 print(message.sid)
